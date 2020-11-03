300355a874ebcc239c6ea2d9f5d13ff797661586
Shoaib32 <shoaibmansoori32@gmail.com>
2020-11-03 22:38:24 +0530

    Tour of Heroes: Add Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HeroesComponent } from "./heroes/heroes.component";
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
+import { HeroService } from "./hero.service";
+import { MessageService } from "./message.service";
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
+  bootstrap: [AppComponent],
+  providers: [HeroService, MessageService]
 })
 export class AppModule {}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Hero } from "../hero";
+import { HeroService } from "../hero.service";
+import { MessageService } from "../message.service";
 import { HEROES } from "../mock-heroes";
 
 @Component({
@@ -8,14 +10,26 @@ import { HEROES } from "../mock-heroes";
   styleUrls: ["./heroes.component.css"]
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() {}
+  constructor(
+    private readonly heroService: HeroService,
+    private readonly messageService: MessageService
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(
+      `HeroesComponent: Selected hero id = ${this.selectedHero.id}`
+    );
   }
 }
